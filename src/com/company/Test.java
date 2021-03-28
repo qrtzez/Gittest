19869bc62e2f49d65be1ae44ee3dfa1e220888a9
qrtz <qrtz@mail.ru>
2021-03-28 23:51:12 +0300

    init map

--- src/com/company/Test.java
+++ src/com/company/Test.java
@@ -1,8 +1,11 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Test {
     List<Integer> list = new ArrayList<>();
+    Map<String, String> map = new HashMap<>();
 }
